a83d4a7e020db74ffec336bff51cf27bfc902618
Mohit choudhary <mohit.choudhary@sap.com>
2024-01-27 20:08:38 +0000

    CI

--- services/posts/index.js
+++ services/posts/index.js
@@ -14,8 +14,8 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
-  const id = randomBytes(4).toString("hex");
+app.post('/posts/create', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
   posts[id] = {
@@ -23,7 +23,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
